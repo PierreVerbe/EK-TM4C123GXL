aa2d5aeb51671fa0cf98fff9c31154dfb53cf18c
Pierre Verbe <pierre.verbe@free.fr>
2019-05-10 10:13:11 +0200

    Début menu + modif LM35

--- Mission 3/Menu/menu/menu.ino
+++ Mission 3/Menu/menu/menu.ino
@@ -3,6 +3,10 @@ float temperature;
 
 void setup() {
 Serial.begin(9600);
+
+Serial1.begin(9600);
+Serial1.write("AT+NAMEmichel");
+Serial1.read();
 }
 
 void loop() {
@@ -10,4 +14,10 @@ void loop() {
   temperature = map(temperature, 0, 4095, 0, 3300);
   temperature = temperature/10;
   Serial.println(temperature);
+
+  int inByte = Serial1.read();
+  Serial1.write("Température : "); 
+  Serial1.print(temperature); 
+  Serial1.write(" °C"); 
+  delay(100);
 }
