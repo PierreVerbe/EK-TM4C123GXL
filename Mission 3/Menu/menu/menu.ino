90037b208f4bca1978f234d911facb2dd3962ab9
pablogra <pablo.grana@isep.fr>
2019-05-13 09:15:35 +0200

    modif menu

--- Mission 3/Menu/menu/menu.ino
+++ Mission 3/Menu/menu/menu.ino
@@ -1,6 +1,7 @@
 int pinLM35 = 26;
 int pinPhoto = 25;
 float temperature;
+float luminosite;
 
 void setup() {
 Serial.begin(9600);
@@ -11,15 +12,10 @@ Serial1.read();
 }
 
 long menu() {
-  Serial1.println("Veuillez entrer le chiffre du service souhaité.");
-  Serial1.println("1. Afficher la température.");
-  Serial1.println("2. Affichaer la luminosité."); 
-  Serial1.println("3. Action sur l'actionneur."); 
-  Serial1.println("4. Afficher la distance."); 
-  Serial1.println("0. Quitter.");
+
   long choix = -1;
 
-  while(1){
+  while(choix == -1){
     choix = Serial1.read(); 
     
     if(choix == 48) return 0;
@@ -45,15 +41,35 @@ void getTemp() {
   delay(250);
 }
 
-void loop() {
+void getLum(){
+  luminosite = analogRead(pinPhoto);
+  luminosite = map(luminosite, 0, 4095, 0, 100);
+  Serial1.write("Luminosité : ");
+  Serial1.print(luminosite);
+  Serial1.write(" %\n");
+  delay(250);
+}
 
-    if(menu() == 1){
-      Serial1.println("AFFICHAGE DE LA TEMPERATURE EN COURS"); 
-      getTemp();
-    }
-    else{
+void loop() {
+  Serial1.println("Veuillez entrer le chiffre du service souhaité.");
+  Serial1.println("1. Afficher la température.");
+  Serial1.println("2. Affichaer la luminosité."); 
+  Serial1.println("3. Action sur l'actionneur."); 
+  Serial1.println("4. Afficher la distance."); 
+  Serial1.println("0. Quitter.");
+  
+  if(menu() == 1){
+    Serial1.println("AFFICHAGE DE LA TEMPERATURE EN COURS"); 
+    getTemp();
+  }
+  else if(menu() == 2){
+    Serial1.println("AFFICHAGE DE LA LUMINOSITE EN COURS");
+    getLum();
+  }
+  else{
     Serial1.println("RIEN"); 
-    }
+  }
+
     
 
 //photoReading = analogRead(pinPhoto);
