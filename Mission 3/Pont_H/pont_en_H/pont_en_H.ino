6b6e33f3cfdd95f0e5505efd707d0b3e5cf4f441
Pierre Verbe <pierre.verbe@free.fr>
2019-05-20 09:49:46 +0200

    Finalisation code pont en H

--- Mission 3/Pont_H/pont_en_H/pont_en_H.ino
+++ Mission 3/Pont_H/pont_en_H/pont_en_H.ino
@@ -1,8 +1,8 @@
 //int pinH_sens = 3; //PB_0
 //int pinH_inverse = 4; //PB_1
 
-int pinH_sens = 24;
-int pinH_inverse = 26;
+int pinH_sens = 15; //26
+int pinH_inverse = 7; //22
 
 void setup() {
   Serial.begin(9600);
@@ -11,11 +11,13 @@ void setup() {
 void loop() {
   Serial.println("START moteur 50% bon sens");
   analogWrite(pinH_inverse, 0);
-  analogWrite(pinH_sens, 510);
+  analogWrite(pinH_sens, 255);
   delay(5000); 
 
   Serial.println("START moteur 50% autre sens");
   analogWrite(pinH_sens, 0);
-  analogWrite(pinH_inverse, 510);
+  analogWrite(pinH_inverse, 255);
   delay(5000); 
 }
+
+//18
